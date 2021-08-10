89c7e07d0d4b729c5d682150722062a6e7818235
maghazi1442 <83565356+maghazi1442@users.noreply.github.com>
2021-08-11 04:19:39 +0700

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -50,7 +50,7 @@ async def ytdl(_, message):
     except Exception as e:
         print(e)
         try:
-            thumbnail_url = "https://telegra.ph/file/ce37f8203e1903feed544.png"
+            thumbnail_url = "https://telegra.ph/file/5fa0bf22549c89cfc3013.jpg"
             await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
         except Exception as e:
             await sentm.edit(
