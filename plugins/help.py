153869779acaf35ade766c199974634778679998
maghazi1442 <83565356+maghazi1442@users.noreply.github.com>
2021-08-19 02:25:31 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"kirim link video youtube bot ini, insya' Allah kami akan mengunduh"
+    helptxt = f"kirim link video youtube ke bot ini, insya' Allah kami akan bantu mengunduh"
     await message.reply_text(helptxt)
